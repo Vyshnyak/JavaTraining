13b1553ff0966d777d157ee15f04e01d39d7d768
Maksym Vyshnyak <dbiyzr13@gmail.com>
2018-03-12 11:41:42 +0200

    changed the ukrainian regex in task03

--- Task03/src/ua/training/homework/controller/RegExp.java
+++ Task03/src/ua/training/homework/controller/RegExp.java
@@ -4,21 +4,21 @@ package ua.training.homework.controller;
  * Regular Expression patterns
  */
 public interface RegExp {
-    String LAST_NAME = "^[A-Za-zА-Яа-я'іІїЇ]+$";
-    String FIRST_NAME = "^[A-Za-zА-Яа-я'іІїЇ]+$";
-    String MIDDLE_NAME = "^[A-Za-zА-Яа-я'іІїЇ]+$";
-    String NICKNAME = "^([A-Za-zА-Яа-я'іІїЇ]+[0-9-_\\.])*[A-Za-zА-Яа-я'іІїЇ]+[0-9]*$";
-    String COMMENT = "^([\\wА-Яа-я'іІїЇ]+\\s)*[\\wА-Яа-я'іІїЇ]+$";
+    String LAST_NAME = "^[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
+    String FIRST_NAME = "^[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
+    String MIDDLE_NAME = "^[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
+    String NICKNAME = "^(A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[0-9-_\\.])*[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[0-9]*$";
+    String COMMENT = "^([\\wА-ЩЬЮЯІЇЄа-щьюяіїє']+\\s)*[\\wА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
     String GROUP = "(Friends|Colleagues|Siblings)";
     String HOME_PHONE = "^\\+?\\d*(\\(\\d{3}\\))?(\\d+-?){0,2}\\d+$";
     String CELL_PHONE_ONE = "^\\+?\\d*(\\(\\d{3}\\))?(\\d+-?){0,2}\\d+$";
     String SECOND_CELL_TWO = "(^\\+?\\d*(\\(\\d{3}\\))?(\\d+-?){0,2}\\d+)?$";
     String EMAIL = "^([\\w!#$%&'*+\\-/=?^`{|}~]+\\.)*[\\w!#$%&'*+\\-/=?^`{|}~]+@([A-Za-z0-9]+\\-)*[A-Za-z0-9]+\\.(com|ua|net|ru)$";
-    String SKYPE = "^([A-Za-zА-Яа-я'іІїЇ]+[0-9-_\\.])*[A-Za-zА-Яа-я'іІїЇ]+[0-9]*$";
+    String SKYPE = "^([A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[0-9-_\\.])*[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[0-9]*$";
     String INDEX = "^\\d+$";
-    String CITY = "^([A-Za-zА-Яа-я'іІїЇ]+\\-)*[A-Za-zА-Яа-я'іІїЇ]+$";
-    String STREET = "^([A-Za-zА-Яа-я'іІїЇ]+[-.]\\s)*[A-Za-zА-Яа-я'іІїЇ]+[-\\s]?[A-Za-zА-Яа-я'іІїЇ]+$";
-    String HOUSE = "^([\\d])+/?[A-Za-zА-Яа-яіІїЇ]?$";
+    String CITY = "^([A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+\\-)*[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
+    String STREET = "^([A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[-.]\\s)*[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+[-\\s]?[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']+$";
+    String HOUSE = "^([\\d])+/?[A-Za-zА-ЩЬЮЯІЇЄа-щьюяіїє']?$";
     String APARTMENT = "^\\d+$";
 
     String LANGUAGE = "^(en|ua)$";
